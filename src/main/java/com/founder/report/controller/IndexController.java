8482b176121052e94c3cca59930c22a9645655e7
founder <747060040@qq.com>
2018-01-03 16:29:40 +0800

    框架提交

--- src/main/java/com/founder/report/controller/IndexController.java
+++ src/main/java/com/founder/report/controller/IndexController.java
@@ -3,13 +3,16 @@ package com.founder.report.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+/**
+ *
+ */
 @Controller
 public class IndexController {
-    @RequestMapping("/index")
-    public String index(Model model) {
-        model.addAttribute("hello", "张三");
-        return "test1";
+    @RequestMapping(value = {"/"},method = RequestMethod.GET)
+    public String index() {
+        return "index";
     }
 
 }
